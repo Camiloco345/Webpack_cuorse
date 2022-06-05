4eba657f4ab13dabc8af39857af796de174fefce
Camilo Ovalle <camilo.erique.ovalleqgmail.com>
2022-06-04 21:15:46 -0500

    Agregando la configuracion de las fuentes al proyecto; La verdad esta parte el algo compleja, y no creo haber entendido todo pero bueo, lintentare despues

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: 'asssts/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
     },
-
+//sección de MODULOS
     module:{
+        //Sección de REGLAS de los MODULOS
         rules: [
         {
             test: /\.m?js$/,
@@ -33,9 +35,25 @@ module.exports = {
         {
             test: /\.png$/,
             type: 'asset/resource'
+        },
+        {
+            test: /\.(woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "./assets/fonts/",
+                    esModule: false,
+                }
+            }
+               
         }
     ]  
     },
+    //Sección de PLUGINS
     plugins:[
         new HtmlWebpackPlugin({
             inject: true,
